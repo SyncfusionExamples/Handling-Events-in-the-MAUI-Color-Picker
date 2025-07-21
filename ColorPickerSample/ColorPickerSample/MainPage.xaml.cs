94943129089514edb4e2c1c9800498ecc83adf7e
snehakPV <sneha.sf4154@gmail.com>
2025-07-21 15:37:34 +0530

    Prepared sample and read me file for the Color Picker control

---
+++ ColorPickerSample/ColorPickerSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.Maui.Inputs;
+
+namespace ColorPickerSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnColorChanging(object sender, ColorChangingEventArgs e)
+        {
+            colorPicker1.SelectedColor = e.NewColor;
+        }
+
+        private void OnColorChanged(object sender, ColorChangedEventArgs e)
+        {
+            colorPicker2.SelectedColor = e.NewColor;
+        }
+
+        private void OnColorSelected(object sender, ColorSelectedEventArgs e)
+        {
+            label.BackgroundColor = e.SelectedColor;
+            label.Text = e.SelectedColor.ToHex();
+        }
+    }
+}
